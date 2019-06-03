b00d313d0a00b31c9496270d8721a527fba7e024
tommaso auerbach <tsa302@nyu.edu>
2019-06-03 05:46:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,7 +40,7 @@ shipping_manifest["muskets"] = 2
   # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest["gun powder"] = 4
 
-
+end
   # return the shipping_manifest hash below
 puts shipping_manifest
 end
